505dbcfed6a2d01ee243ae0250678c1ea9706012
pablitohaddad <pablohaddad73@gmail.com>
2024-08-16 19:55:38 -0400

    Firsts statps in java with Spring Boot 3

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-springboot-and-java/src/main/java/io/github/pablitohaddad/restwithspringbootandjava/GreetingController.java
@@ -0,0 +1,21 @@
+package io.github.pablitohaddad.restwithspringbootandjava;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController// Combination of ResponseBody and Controller;
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong(); // Id mock
+
+    @RequestMapping("/greeting") // mapeia a requisição
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "World") String name){ // parametros opcionais
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
