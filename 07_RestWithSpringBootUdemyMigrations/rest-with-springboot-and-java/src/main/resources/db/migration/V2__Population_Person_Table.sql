01be5b101c34a498355ddcd523014d9da11156b8
pablitohaddad <pablohaddad73@gmail.com>
2024-08-24 16:42:26 -0400

    migrations in java

---
+++ 07_RestWithSpringBootUdemyMigrations/rest-with-springboot-and-java/src/main/resources/db/migration/V2__Population_Person_Table.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (id, address, first_name, gender, last_name) VALUES
+                                                                      (1, 'Cáceres-MT', 'Pablo', 'Male', 'Haddad'),
+                                                                      (2, 'Cuiaba-MT', 'Murilo', 'Male', 'Ramos'),
+                                                                      (3, 'Cáceres-MT', 'Sophia', 'Famale', 'Vargas'),
+                                                                      (4, 'Cáceres-MT', 'Andrea', 'Female', 'Eva');
