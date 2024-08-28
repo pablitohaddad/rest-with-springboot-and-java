e2796172178a62ca2a1c385dab602666ef3509c5
pablitohaddad <pablohaddad73@gmail.com>
2024-08-28 17:41:43 -0400

    Content Negotiation

--- 09_RestWithSpringBootUdemy Content Negociation/rest-with-springboot-and-java/src/main/java/io/github/pablitohaddad/restwithspringbootandjava/controllers/PersonController.java
+++ 09_RestWithSpringBootUdemy Content Negociation/rest-with-springboot-and-java/src/main/java/io/github/pablitohaddad/restwithspringbootandjava/controllers/PersonController.java
@@ -2,8 +2,8 @@ package io.github.pablitohaddad.restwithspringbootandjava.controllers;
 
 import io.github.pablitohaddad.restwithspringbootandjava.data.vo.v1.PersonVO;
 import io.github.pablitohaddad.restwithspringbootandjava.services.PersonServices;
+import io.github.pablitohaddad.restwithspringbootandjava.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,14 +16,16 @@ public class PersonController {
     @Autowired
     private PersonServices services;
 
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public PersonVO create(@RequestBody PersonVO person){
         return services.create(person);
     }
 
-    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public PersonVO update(@RequestBody PersonVO person){
         return services.update(person);
     }
@@ -35,12 +37,12 @@ public class PersonController {
     }
 
     @GetMapping(value = "/{id}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonVO findById(@PathVariable("id") Long id) throws Exception{
         return services.findById(id);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public List<PersonVO> findAll(){
         return services.findAll();
     }
---
+++ 09_RestWithSpringBootUdemy Content Negociation/rest-with-springboot-and-java/src/main/java/io/github/pablitohaddad/restwithspringbootandjava/util/MediaType.java
@@ -0,0 +1,10 @@
+package io.github.pablitohaddad.restwithspringbootandjava.util;
+
+public class MediaType {
+
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String APPLICATION_YML = "application/xml";
+    public static final String APPLICATION_XML = "application/x-yaml";
+
+
+}
